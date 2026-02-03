54510c711cae45f8e56ca1fdb7e55f9fc1e27625
Gabriela Araujo <gabyara237@gmail.com>
2026-02-03 00:25:22 -0800

    feat: implement guess_number level up exercise

--- exercises.py
+++ exercises.py
@@ -242,3 +242,67 @@ def determine_season():
 
 # Call the function
 determine_season()
+
+
+# Exercise 6: Number Guessing Game
+#
+# Write a Python function named `guess_number` that allows a user to guess a predetermined number within a range.
+#
+# Requirements:
+# - Set a fixed number as the target for guessing (e.g., 42).
+# - Prompt the user to guess a number within a range (e.g., 1 to 100).
+# - Allow the user to guess up to five times.
+# - After each guess, use conditional statements with AND, OR, and NOT to give the user hints like:
+#   - "Guess is too low" or "Guess is too high."
+#   - "Last chance!" when they are on their fifth guess.
+# - Print "Congratulations, you guessed correctly!" if they guess the number.
+# - Print "Sorry, you failed to guess the number in five attempts." if they do not succeed.
+#
+# Hints:
+# - Use a for loop with a range to limit guesses to five.
+# - Use logical AND, OR, and NOT to check conditions and provide appropriate feedback.
+
+def guess_number():
+    # Your control flow logic goes here
+
+    TARGET= 22
+    user_attempt=5
+    winner = False
+
+
+    while user_attempt>=1 and not winner:
+        if user_attempt==1:
+            print("Last chance!\n")
+        
+        user_input = input(f"Guess a number from 1 to 50. You have {user_attempt} attempts.\n")
+
+        if not user_input.isdigit():
+            print("Sorry, invalid entry")
+            continue
+        
+        int_user_input = int(user_input)
+
+        if int_user_input < 1 or int_user_input > 50:
+            print("Sorry, invalid entry")
+            continue
+
+        if int_user_input == TARGET:
+            print("Congratulations, you guessed correctly! ")
+            winner = True
+            break
+
+
+        if int_user_input >TARGET:
+            print("Guess is too high.")
+
+        if int_user_input <TARGET:
+            print( "Guess is too low.")
+
+        user_attempt -=1
+
+    if not winner:
+        print ("Sorry, you failed to guess the number in five attempts.")
+
+# Call the function
+guess_number()
+
